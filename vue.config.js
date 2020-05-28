ffce1a84ba74083be3175b0335195997cead6e08
dolly_zhang <52lyyzsq>
2020-05-28 17:33:29 +0800

    init project

---
+++ vue.config.js
@@ -0,0 +1,45 @@
+const data = require('./data.json')
+const seller = data.seller
+const ratings = data.ratings
+const goods = data.goods
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        import: [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+    }
+  }
+}
