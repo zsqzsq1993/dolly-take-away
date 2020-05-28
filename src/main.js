48a750e76bad2cedb5521b03eb4288e96fabc7fe
dolly_zhang <52lyyzsq>
2020-05-29 00:47:06 +0800

    change host in main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './assets/css/global/index.scss'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios.create({
-  baseURL: 'http://106.53.118.169/:8080/api' // 本地开发localhost
+  baseURL: 'http://106.53.118.169:8080/api' // 本地开发localhost
 })
 
 new Vue({
